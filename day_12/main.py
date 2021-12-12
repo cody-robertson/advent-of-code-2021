ec028510b680e10947ed8ccad063a9be14ce92e0
Cody Robertson <codyallanrobertson@gmail.com>
2021-12-12 13:09:06 -0600

    clean day 12 solutions

--- day_12/main.py
+++ day_12/main.py
@@ -3,58 +3,45 @@ from typing import Optional
 
 
 def load_input(file_name: str):
-    graph = defaultdict(list)
+    graph = defaultdict(set)
     with open(file_name) as input_file:
         for line in input_file:
             first, second = line.strip().split("-")
-            graph[first].append(second)
-            graph[second].append(first)
+            graph[first].add(second)
+            graph[second].add(first)
     return graph
 
 
-def dfs_all_paths(graph: dict[str, list[str]], path: Optional[list[str]] = None):
-    if path is None:
-        path = ["start"]
-    previous = path[-1]
-
-    if previous == "end":
-        return [path]
-
-    paths = []
-    children = graph[previous]
-    for child in children:
-        if child != "start" and child not in path or child.isupper():
-            paths.extend(dfs_all_paths(graph, path + [child]))
-    return paths
-
-
-def dfs_all_paths_additional_trip(
-    graph: dict[str, list[str]],
-    path: Optional[list[str]] = None,
+def dfs_all_paths(
+    graph: dict[str, set[str]],
+    current: str = "start",
+    path: Optional[set[str]] = None,
     has_visited_twice=False,
 ):
     if path is None:
-        path = ["start"]
-    previous = path[-1]
+        path = {current}
 
-    if previous == "end":
+    if current == "end":
         return [path]
 
     paths = []
-    children = graph[previous]
+    children = graph[current]
     for child in children:
-        child_occurrences = len([item for item in path if item == child])
-        if child != "start" and (child_occurrences < 1 or child.isupper()):
-            paths.extend(
-                dfs_all_paths_additional_trip(graph, path + [child], has_visited_twice)
+        if child != "start" and (
+            child not in path or child.isupper() or not has_visited_twice
+        ):
+            has_visited_child_twice = has_visited_twice or (
+                child in path and not child.isupper()
             )
-        elif child != "start" and child_occurrences == 1 and not has_visited_twice:
-            paths.extend(dfs_all_paths_additional_trip(graph, path + [child], True))
+            paths.extend(
+                dfs_all_paths(
+                    graph, child, path.union({child}), has_visited_child_twice
+                )
+            )
     return paths
 
 
 if __name__ == "__main__":
     file_input = load_input("input.txt")
-    print(file_input)
-    print(len(dfs_all_paths(file_input)))
-    print(len(dfs_all_paths_additional_trip(file_input)))
+    print("part 1:", len(dfs_all_paths(file_input, has_visited_twice=True)))
+    print("part 2:", len(dfs_all_paths(file_input)))
